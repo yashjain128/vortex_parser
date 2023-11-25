9723c6b6cdcfb6e89c71cfc08e9330444c46d742
Yash Jain <ItIsYashJain@gmail.com>
2023-11-25 13:26:32 -0800

    Moved scrolling plot widget to plotting.py

--- src/plotting.py
+++ src/plotting.py
@@ -9,14 +9,11 @@ import math
 
 import numpy as np
 from vispy import scene, plot, app
-from vispy.visuals.transforms import STTransform
+from vispy.visuals.transforms import STTransform, MatrixTransform
 
 from pymap3d.ecef import ecef2geodetic, ecef2enuv
 
 from scipy.io import loadmat
-
-from scrollingplotwidget import ScrollingPlotWidget
-
 
 SYNC = [64, 40, 107, 254]
 MINFRAME_LEN = 2 * 40
@@ -80,111 +77,6 @@ def find_RV(seq):
     check = candidates[:, np.newaxis] + np.arange(5)
     mask = np.all((np.take(seq, check) == rv_arr), axis=-1)
     return candidates[mask]
-
-class Channel:
-    def __init__(self, color, signed, numpoints, *raw_byte_info):
-        self.signed = signed
-        self.color = color
-
-        bit_num = 0
-        self.byte_info = []
-
-        for i in range(len(raw_byte_info), 0, -2): 
-            # index and mask are given
-            # infer bit shift from first bit in mask and how many bits have passed
-            
-            ind = raw_byte_info[i-2]
-            mask = raw_byte_info[i-1]
-            shift = int(bit_num - math.log2(mask & -mask))
-
-            self.byte_info.append([ind, mask, shift])            
-            bit_num += mask.bit_count()
-
-        self.xlims = [0, numpoints]
-        # Infer y limits from number of bits
-        if self.signed:
-            self.ylims = [-2**(bit_num-1), 2**(bit_num-1)]
-        else:
-            self.ylims = [0, 2**bit_num]
-
-        self.datay = np.zeros(numpoints)
-        self.datax = np.arange(numpoints)
-
-        self.line = scene.Markers(pos=np.transpose(np.array([self.datax, self.datay])), edge_width=0, size=1, face_color=self.color, antialias=False)
-
-    def new_data(self, minframes):
-        l = len(minframes)
-        self.datay[:l] = np.zeros(l)
-        for ind, mask, shift in self.byte_info:
-            if shift < 0:
-                self.datay[:l] += (minframes[:, ind] & mask) >> abs(shift)
-            else:
-                self.datay[:l] += (minframes[:, ind] & mask) << shift
-        
-        if self.signed:
-            self.datay[:l] = self.datay[:l]+(self.datay[:l] >= self.ylims[1])*(2*self.ylims[0])
-        self.datay = np.roll(self.datay, -l)
-
-        data = np.transpose(np.array([self.datax, self.datay]))
-        self.line.set_data(pos=data, edge_width=0, size=1, face_color=self.color)
-
-    def reset(self):
-        self.datay = np.zeros(self.xlims[1])
-        self.line.set_data(pos=np.transpose(np.array([self.datax, self.datay])), edge_width=0, size=1, face_color=self.color)
-
-class Housekeeping:
-    def __init__(self, board_id, length, numpoints, b_ind, b_mask, values):
-
-        self.b_ind, self.b_mask = b_ind, b_mask 
-        self.rate = self.b_mask.bit_count()/8
-
-        if self.rate == 8/8: 
-            self.board_id = board_id
-        elif self.rate == 4/8:
-            self.board_id = [board_id>>4, board_id&0xF]
-        else:
-            raise ValueError("Unsupported housekeeping rate")
-        self.numpoints = numpoints*self.rate
-        self.length = 11//self.rate
-
-        self.indcol = np.array(np.arange(11)//self.rate, dtype=np.uint8)[:, None]
-        self.data = np.zeros((11, numpoints))
-        self.values = values
-        self.maxhkrange = 10
-
-    def new_data(self, minframes):
-        databuffer = minframes[:, self.b_ind] & self.b_mask
-        if self.rate == 8/8:
-            inds = np.where(databuffer == self.board_id)[0]
-            inds = inds[np.where(np.diff(inds) == self.length)[0]]
-            if inds.size != 0:
-                self.data[:, :inds.size] = databuffer[self.indcol + inds]
-
-        elif self.rate == 4/8:
-            inds = np.where( (databuffer==self.board_id[0])[:-1] & (databuffer==self.board_id[1])[1:])[0]
-            inds = inds[np.where(np.diff(inds) == self.length)[0]][:-1]
-            self.data[:, :inds.size] = databuffer[self.indcol + inds]<<4 | databuffer[self.indcol+1 + inds]
-
-        #if do_hkunits:
-        #    print(do_hkunits)
-        #    self.data[:, :inds.size] = HK_COEF * (self.data[:, :inds.size]*2.5/256 - 0.5*2.5/256) + HK_ADD
-        
-        self.data = np.roll(self.data, -inds.size, axis=1)
-        hkrange = min(self.maxhkrange, inds.size)
-
-
-
-        for edit, data_row in zip(self.values, self.data):
-            if edit.isEnabled():
-                if hkrange==0:
-                    edit.setText("null")
-                else:
-                    edit.setText(f"{np.average(data_row[hkrange:]): .{DEC_PLACES}f}")
-
-    
-    def reset(self):
-        for value in self.values:
-            value.setText("")
 
 def set_hkunits(hkunits):
     global do_hkunits
@@ -294,6 +186,9 @@ def reset_graphs():
     for obj_arr in data_channels.values():
         for obj in obj_arr:
             obj.reset()
+    
+    for _gps in gps_data.values():
+        _gps.fill(0)
     
 
 def set_map(map_file):
@@ -443,3 +338,276 @@ def parse(read_mode, plot_hertz, read_file, udp_ip, udp_port):
             # Change pause time with threading?
             time.sleep(pause_time)
             start_time = time.perf_counter()
+
+class Channel:
+    def __init__(self, color, signed, numpoints, *raw_byte_info):
+        self.signed = signed
+        self.color = color
+
+        bit_num = 0
+        self.byte_info = []
+
+        for i in range(len(raw_byte_info), 0, -2): 
+            # index and mask are given
+            # infer bit shift from first bit in mask and how many bits have passed
+            
+            ind = raw_byte_info[i-2]
+            mask = raw_byte_info[i-1]
+            shift = int(bit_num - math.log2(mask & -mask))
+
+            self.byte_info.append([ind, mask, shift])            
+            bit_num += mask.bit_count()
+
+        self.xlims = [0, numpoints]
+        # Infer y limits from number of bits
+        if self.signed:
+            self.ylims = [-2**(bit_num-1), 2**(bit_num-1)]
+        else:
+            self.ylims = [0, 2**bit_num]
+
+        self.datay = np.zeros(numpoints)
+        self.datax = np.arange(numpoints)
+
+        self.line = scene.Markers(pos=np.transpose(np.array([self.datax, self.datay])), edge_width=0, size=1, face_color=self.color, antialias=False)
+
+    def new_data(self, minframes):
+        l = len(minframes)
+        self.datay[:l] = np.zeros(l)
+        for ind, mask, shift in self.byte_info:
+            if shift < 0:
+                self.datay[:l] += (minframes[:, ind] & mask) >> abs(shift)
+            else:
+                self.datay[:l] += (minframes[:, ind] & mask) << shift
+        
+        if self.signed:
+            self.datay[:l] = self.datay[:l]+(self.datay[:l] >= self.ylims[1])*(2*self.ylims[0])
+        self.datay = np.roll(self.datay, -l)
+
+        data = np.transpose(np.array([self.datax, self.datay]))
+        self.line.set_data(pos=data, edge_width=0, size=1, face_color=self.color)
+
+    def reset(self):
+        self.datay = np.zeros(self.xlims[1])
+        self.line.set_data(pos=np.transpose(np.array([self.datax, self.datay])), edge_width=0, size=1, face_color=self.color)
+
+class Housekeeping:
+    def __init__(self, board_id, length, numpoints, b_ind, b_mask, values):
+
+        self.b_ind, self.b_mask = b_ind, b_mask 
+        self.rate = self.b_mask.bit_count()/8
+
+        if self.rate == 8/8: 
+            self.board_id = board_id
+        elif self.rate == 4/8:
+            self.board_id = [board_id>>4, board_id&0xF]
+        else:
+            raise ValueError("Unsupported housekeeping rate")
+        self.numpoints = int(numpoints*self.rate)
+        self.length = 11//self.rate
+
+        self.indcol = np.array(np.arange(11)//self.rate, dtype=np.uint8)[:, None]
+        self.data = np.zeros((11, self.numpoints))
+        self.values = values
+        self.maxhkrange = 10
+
+    def new_data(self, minframes):
+        databuffer = minframes[:, self.b_ind] & self.b_mask
+        if self.rate == 8/8:
+            inds = np.where(databuffer == self.board_id)[0]
+            inds = inds[np.where(np.diff(inds) == self.length)[0]]
+            if inds.size != 0:
+                self.data[:, :inds.size] = databuffer[self.indcol + inds]
+
+        elif self.rate == 4/8:
+            inds = np.where( (databuffer==self.board_id[0])[:-1] & (databuffer==self.board_id[1])[1:])[0]
+            inds = inds[np.where(np.diff(inds) == self.length)[0]][:-1]
+            self.data[:, :inds.size] = databuffer[self.indcol + inds]<<4 | databuffer[self.indcol+1 + inds]
+
+        #if do_hkunits:
+        #    print(do_hkunits)
+        #    self.data[:, :inds.size] = HK_COEF * (self.data[:, :inds.size]*2.5/256 - 0.5*2.5/256) + HK_ADD
+        
+        self.data = np.roll(self.data, -inds.size, axis=1)
+        hkrange = min(self.maxhkrange, inds.size)
+
+
+
+        for edit, data_row in zip(self.values, self.data):
+            if edit.isEnabled():
+                if hkrange==0:
+                    edit.setText("null")
+                else:
+                    edit.setText(f"{np.average(data_row[hkrange:]): .{DEC_PLACES}f}")
+
+    
+    def reset(self):
+        self.data = np.zeros((11, self.numpoints))
+        for value in self.values:
+            value.setText("")
+
+class ScrollingPlotWidget(scene.Widget):
+    """
+    Widget for 2d and 3d plots built on top of scene.Widget.
+
+    To use this with Fig changing  the default class is necessary
+    """
+    def __init__(self, *args, **kwargs):
+        self.grid = None
+        self.plot_grid = None
+        self.plot_view = None
+        self.camera = None
+        self.title = None
+        self.title_widget = None
+        self.yaxis, self.xaxis, self.zaxis = None, None, None
+        self.ylabel, self.xlabel, self.zlabel = None, None, None
+        self.ylims, self.xlims, self.zlims = None, None, None
+        self.dimensions = None
+        self.data = None
+        self.view_grid = None
+        self._configured = False
+        self.visuals = []
+        self.section_y_x = None
+
+        super(ScrollingPlotWidget, self).__init__(*args, **kwargs)
+        self.grid = self.add_grid(spacing=0, margin=10)
+        
+
+    def configure2d(self, title, xlabel, ylabel, xlims=[0, 1], ylims=[-1, 1]):
+
+        fg = "#000000"
+        self.xlims = xlims[:]
+        self.ylims = ylims[:]
+
+        self.dimensions = 2
+
+        # padding left
+        padding_left = self.grid.add_widget(None, row=0, row_span=3, col=0)
+        padding_left.width_min = 5
+        padding_left.width_max = 10
+
+        # padding right
+        padding_right = self.grid.add_widget(None, row=0, row_span=3, col=3)
+        padding_right.width_min = 15
+        padding_right.width_max = 25
+
+        # padding down
+        padding_bottom = self.grid.add_widget(None, row=3, col=0, col_span=4)
+        padding_bottom.height_min = 15
+        padding_bottom.height_max = 15
+
+        # row 0
+        # title - column 4 to 5
+        self.title = scene.Label(title, font_size=8, color="#000000")
+        self.title_widget = self.grid.add_widget(self.title, row=0, col=2)
+        self.title_widget.height_min = 35
+        self.title_widget.height_max = 35
+
+        # row 1
+        # yaxis - column 1
+        # view - column 2
+
+        self.yaxis = scene.AxisWidget(orientation='left',
+                                      text_color=fg,
+                                      axis_color=fg,
+                                      tick_color=fg,
+                                      axis_width=2,
+                                      minor_tick_length=4, 
+                                      major_tick_length=6,
+                                      tick_font_size=6,
+                                      axis_font_size=6,
+                                      axis_label=ylabel,
+                                      axis_label_margin=14,
+                                      tick_label_margin=2)
+        yaxis_widget = self.grid.add_widget(self.yaxis, row=1, col=1)
+        yaxis_widget.width_min = 20
+        yaxis_widget.width_max = 50
+        yaxis_widget.height_min = 50
+
+        # xaxis - column 3
+        self.xaxis = scene.AxisWidget(orientation='bottom', 
+                                      text_color=fg,
+                                      axis_color=fg,
+                                      axis_label=xlabel,
+                                      axis_font_size=6,
+                                      axis_label_margin=26,
+                                      tick_color=fg,
+                                      tick_font_size=6,
+                                      tick_label_margin=20,
+                                      minor_tick_length=2,
+                                      major_tick_length=5)
+        xaxis_widget = self.grid.add_widget(self.xaxis, row=2, col=2)
+        xaxis_widget.height_max = 30
+        xaxis_widget.width_min = 50
+
+        # This needs to be added to the grid last (to fix #1742)
+        self.plot_view = self.grid.add_view(row=1, col=2, border_color='grey', bgcolor="#efefef") 
+        self.plot_view.camera = 'panzoom'
+        self.camera = self.plot_view.camera
+        self.camera.set_range(x=self.xlims, y=self.ylims)
+
+        self._configured = True
+        self.xaxis.link_view(self.plot_view)
+        self.yaxis.link_view(self.plot_view)
+
+        return self
+
+    def configure3d(self, title, xlabel, ylabel, zlabel, xlims=[0, 1], ylims=[0, 1], zlims=[0, 1]):
+
+        self.dimensions = 3
+        fg = "#000000"
+        self.plot_view = self.grid.add_view(row=1, col_span=3, col=1, border_color='grey', bgcolor="#efefef")
+        self.plot_view.camera = 'turntable'
+        self.plot_view.camera.center = (0.5, 0.5, 0.5)
+        # self.plot_view.camera.fov = 80
+        #.center((0.5, 0.5, 0.5))   
+        self.camera = self.plot_view.camera
+        
+        #return
+        self.ylims = ylims
+        self.xlims = xlims
+
+        # padding left
+        padding_left = self.grid.add_widget(None, row=1, row_span=2, col=0)
+        padding_left.width_min = 20
+        padding_left.width_max = 20
+
+        # padding right
+        padding_right = self.grid.add_widget(None, row=1, row_span=2, col=2)
+        padding_right.width_min = 20
+        padding_right.width_max = 30
+
+        # padding down
+        padding_bottom = self.grid.add_widget(None, row=2, col=0, col_span=3)
+        padding_bottom.height_min = 20
+        padding_bottom.height_max = 20
+
+        # row 0 
+        # title - column 4 to 5
+        self.title = scene.Label(title, font_size=8, color=fg)
+        self.title_widget = self.grid.add_widget(self.title, row=0, col=1, col_span=3)
+        self.title_widget.height_min = 30
+        self.title_widget.height_max = 30    
+
+        self.xaxis = scene.Axis(pos=[[0, 0], [1, 0]], tick_direction=(0, -1), axis_width=0.1, tick_width=1, domain=xlims,
+                                axis_color=fg, tick_color=fg, text_color=fg, font_size=20, parent=self.plot_view.scene)
+        self.yaxis = scene.Axis(pos=[[0, 0], [0, 1]], tick_direction=(-1, 0), axis_width=1, tick_width=1, domain=ylims,
+                                axis_color=fg, tick_color=fg, text_color=fg, font_size=20, parent=self.plot_view.scene)
+        self.zaxis = scene.Axis(pos=[[0, 0], [-1, 0]], tick_direction=(0, -1), axis_width=1, tick_width=1, domain=zlims,
+                                axis_color=fg, tick_color=fg, text_color=fg, font_size=20, parent=self.plot_view.scene)
+        self.zaxis.transform = scene.transforms.MatrixTransform()  # its acutally an inverted xaxis
+        self.zaxis.transform.rotate(90, (0, 1, 0))  # rotate cw around yaxis
+        self.zaxis.transform.rotate(-45, (0, 0, 1))  # tick direction towards (-1,-1)
+        self._configured = True
+
+        return self
+    def add_line(self, line):
+        self.plot_view.add(line) 
+        
+        return line
+    def add_gridlines(self):
+        self.view_grid = scene.visuals.GridLines(color=(0, 0, 0, 0.5))
+        self.view_grid.set_gl_state('translucent')
+        self.plot_view.add(self.view_grid)
+    
+    def reset_bounds(self):
+        self.camera.set_range(x=self.xlims, y=self.ylims)
